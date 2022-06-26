68899be7e2381d4f3f0297928ba3c9e04fa0ef4f
mac Ray <rztechdude@yahoo.com>
2022-06-25 21:50:12 -0400

    resume added sections

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -10,11 +10,19 @@ function Resume() {
                 <span>
                     Download my resume
                 </span>
+                <h4>Front-End Proficiencies</h4>
+                <ul>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>REACT</li>
+                </ul>
+                <h4>Back-End Proficiencies</h4>
+                <ul>
+                    <li>Javascript</li>
+                    <li>NODE</li>
+                    <li>MYSqL/ Sequelize</li>
+                </ul>
             </div>
-            <div>
-                
-            </div>
-
         </section>
     );
 }
